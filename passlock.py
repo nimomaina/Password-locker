a06ea420f5e06e91c2abe9a1afc3d202103e999c
nimomaina <nimomaina.nm@gmail.com>
2019-01-27 13:44:13 +0300

    Create class users

---
+++ passlock.py
@@ -0,0 +1,12 @@
+class Users:
+
+    """
+    Class that generates instances of user data
+    """
+    pass_account = []
+
+    def __init__(self, account, acc_username, acc_pass ):
+
+        self.account = account
+        self.acc_username = acc_username
+        self.acc_pass = acc_pass
