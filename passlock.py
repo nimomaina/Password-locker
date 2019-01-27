cc9092474468e01051feecbaa3bef33b9bacd369
nimomaina <nimomaina.nm@gmail.com>
2019-01-27 16:19:52 +0300

    Put two classes in a single file

--- passlock.py
+++ passlock.py
@@ -10,4 +10,15 @@ class Users:
         self.password = password
 
 
+class Credentials:
 
+    """
+    Class that generates instances of credential data
+    """
+
+    credentials_list = []
+
+    def __init__(self, acc_name, acc_username, acc_password):
+        self.acc_name = acc_name
+        self.acc_username = acc_username
+        self.acc_password = acc_password
