5ec3ff6cda31c1a0c5a42aa52f8d04cae8bb6f11
nimomaina <nimomaina.nm@gmail.com>
2019-01-28 09:51:18 +0300

    Created all functions from credentials class

--- run.py
+++ run.py
@@ -21,10 +21,66 @@ def save_users(user):
 
 def check_exist_user(user_name):
     """
-    Funtion to check for existing users
+    Function to check for existing users
     :param user_name:
     :return: Boolean
     """
     return Users.users_exist(user_name)
+
+
+def authenticate_user(user_name, password):
+    """
+    Function to authenticate users when they login
+    :param user_name:
+    :param password:
+    """
+    return Users.authenticate_user(user_name, password)
+
+
+def create_credentials(acc_name, acc_username, acc_password):
+    """
+    Function that creates new credentials
+    :param acc_name:
+    :param acc_username:
+    :param acc_password:
+    :return: newly created credential
+    """
+    new_credential = Credentials(acc_name, acc_username, acc_password)
+    return new_credential
+
+
+def save_credential(credential):
+    """
+    Function that adds credentials to credential list
+    """
+    credential.save_credentials()
+
+
+def delete_credential(credential):
+    """
+    Function that deletes credentials
+    """
+    credential.delete_credentials()
+
+
+def find_credentials(acc_name):
+    """
+    Function that finds an account using the account name
+    """
+    return Credentials.find_by_account_name(acc_name)
+
+
+def check_existing_credentials(acc_name):
+    """
+    Function that checks if a credential actually exists
+    """
+    return Credentials.credentials_exist(acc_name)
+
+
+def display_credentials():
+    """
+    Function that returns all saved credentials
+    """
+    return Credentials.display_credentials()
 
 
