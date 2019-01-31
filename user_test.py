82bf94b470637b204d14a53f97dbdb9ad426e380
nimomaina <nimomaina.nm@gmail.com>
2019-01-31 16:48:05 +0300

    Edit test for users.

---
+++ user_test.py
@@ -0,0 +1,42 @@
+
+from user import Users
+import unittest
+
+class TestUsers(unittest.TestCase):
+
+    """
+       Test class that defines test cases for the users class behaviours
+
+       Args:
+           unittest.TestCase: TestCase class that helps in creating test cases
+    """
+
+    def setUp(self):
+        """
+        Set up method to run before each test cases.
+        """
+        self.new_user = Users("Judy", "homeboys123")
+
+    def tearDown(self):
+        """
+        cleans up after every test has been done
+        """
+        Users.users_list = []
+
+    def test_init(self):
+        """
+        Test case to test if the object is initialized properly
+        """
+        self.assertEqual(self.new_user.user_name, "Judy")
+        self.assertEqual(self.new_user.password, "homeboys123")
+
+    def test_new_user_creation(self):
+        """
+        Tests if new users account has been created
+        """
+        self.new_user.save_user()
+        self.assertEqual(len(Users.users_list), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
